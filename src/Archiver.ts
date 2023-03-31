dc3ec816d62080cc357b4a4953d371e55f275636
Kevin Gilpin <kgilpin@gmail.com>
2023-03-31 11:02:59 -0400

    fix: Fix git interactions
    Fetch full depth
    Leave .appmap files on the filesystem after commit so they can be archived

--- src/Archiver.ts
+++ src/Archiver.ts
@@ -32,15 +32,16 @@ export default class Archiver {
     if (ref) ref = [ref, `(${revision})`].join(' ');
     else ref = revision;
     for (const command of [
-      `git fetch --depth=1`,
+      `git fetch`,
       `git add .appmap`,
       `git stash`,
       `git checkout ${this.archiveBranch}`,
-      `git stash pop`,
+      `git stash apply`,
       `git add .appmap`,
       `git -c user.name="github-actions[bot]" -c user.email="github-actions[bot]@users.noreply.github.com" commit --author="Author <actions@github.com> " -m "chore: AppMaps for ${ref}"`,
       this.push ? `git push origin ${this.archiveBranch}` : undefined,
       `git checkout ${revision}`,
+      `git stash pop`,
     ].filter(Boolean)) {
       assert(command);
       await executeCommand(command);
